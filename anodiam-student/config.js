9d35e215e1fbfeda546852c668aecbd66f5727ac
Anirban <anodiam.adm@gmail.com>
2022-12-16 16:03:08 +1100

    commit
    commit

---
+++ anodiam-student/config.js
@@ -0,0 +1,19 @@
+// import firebase from 'firebase/compat/app';
+// import 'firebase/compat/auth';
+// import 'firebase/compat/firestore';
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAGaYsy1QXvv3CN0Tdi-7r9FgILXyNSV5w",
+//   authDomain: "anodiam-mvp-c4160.firebaseapp.com",
+//   projectId: "anodiam-mvp-c4160",
+//   storageBucket: "anodiam-mvp-c4160.appspot.com",
+//   messagingSenderId: "226727725631",
+//   appId: "1:226727725631:web:4349753c264e56aa27068a",
+//   measurementId: "G-S5WFPMEY79"
+// };
+
+// if(!firebase.apps.length) {
+//   firebase.initializeApp(firebaseConfig)
+// }
+
+// export { firebase };
---
+++ anodiam-student/src/components/screens/ClientTabs/CoursesScreen.js
@@ -0,0 +1,14 @@
+// import React, { useState, useEffect } from 'react';
+// import { View, Text, StyleSheet, FlatList, Pressable } from 'react-native';
+// import { firebase } from '../../../../config';
+
+// const CoursesScreen = () => {
+//   const [courses, setCourses] = useState([]);
+//   return (
+//     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+//       <Text>My Courses Screen</Text>
+//     </View>
+//   );
+// }
+ 
+// export default CoursesScreen;
